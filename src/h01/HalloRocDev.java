bf35e3bb9641d3c07bd51632843fad9469b7c2ed
oscar <oscarlemstra1@gmail.com>
2020-04-07 15:42:51 +0200

    er is een verandering gemaakt

--- src/h01/HalloRocDev.java
+++ src/h01/HalloRocDev.java
@@ -4,8 +4,7 @@ package h01;
  * Created by piet on 31-08-16.
  */
 public class HalloRocDev {
-
     public static void main(String[] args) {
-        System.out.println("Hallo klas 8A!");
+        System.out.println("Hallo Oscar Lemstra, jij bent nu een roc-dev!");
     }
 }
