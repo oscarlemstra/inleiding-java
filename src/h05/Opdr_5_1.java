3429fac1ef1322bf28ec998be10993b722bff65e
oscar <oscarlemstra1@gmail.com>
2020-04-15 17:01:12 +0200

    opdr 5.1

---
+++ src/h05/Opdr_5_1.java
@@ -0,0 +1,22 @@
+package h05;
+
+import java.awt.*;
+import java.applet.*;
+
+public class Opdr_5_1 extends Applet {
+
+    Color lineColor = Color.black;
+    Color fillColor = Color.magenta;
+    int width = 23;
+    int height = 1;
+
+    public void init () {
+        setSize(700, 400);
+    }
+
+    public void paint (Graphics g) {
+        setBackground(Color.white);
+
+
+    }
+}
