56867f98a51277860cb5bedfcf23958e6b250664
oscar <oscarlemstra1@gmail.com>
2020-04-17 11:58:43 +0200

    opdr 6.1

---
+++ src/h06/Opdr_6_1.java
@@ -0,0 +1,24 @@
+package h06;
+
+import java.awt.*;
+import java.applet.*;
+
+public class Opdr_6_1 extends Applet {
+
+    double verdiendeGeld = 113.00;
+    double bedragPerPersoon;
+
+    public void init () {
+        setSize(400, 300);
+
+        bedragPerPersoon = verdiendeGeld / 4;
+    }
+
+    public void paint (Graphics g) {
+
+        g.drawString("Jan krijgt: € " + bedragPerPersoon + ",-", 30, 50);
+        g.drawString("Ali krijgt: € " + bedragPerPersoon + ",-", 30, 70);
+        g.drawString("Jeannette krijgt: € " + bedragPerPersoon + ",-", 30, 90);
+        g.drawString("Ik krijg: € " + bedragPerPersoon + ",-", 30, 110);
+    }
+}
