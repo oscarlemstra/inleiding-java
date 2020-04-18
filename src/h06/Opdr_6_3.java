1d12b5a016186d06f6cff2df41286fce49cfac82
oscar <oscarlemstra1@gmail.com>
2020-04-18 22:02:14 +0200

    opdr 6.3 af

---
+++ src/h06/Opdr_6_3.java
@@ -0,0 +1,22 @@
+package h06;
+
+import java.awt.*;
+import java.applet.*;
+
+public class Opdr_6_3 extends Applet {
+
+    int a = 2147483647;
+    int b = 5;
+
+    int resultaat;
+
+    public void init () {
+        setSize(400, 300);
+
+        resultaat = a + b;
+    }
+
+    public void paint (Graphics g) {
+        g.drawString("een negatieve int:  " + resultaat, 30, 50);
+    }
+}
