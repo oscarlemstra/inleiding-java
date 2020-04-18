6fad00fe9a729ece151802ab026e23477a6b856d
oscar <oscarlemstra1@gmail.com>
2020-04-18 22:47:12 +0200

    praktijkopdracht 6 af

--- src/h06/PraktijkOpdracht.java
+++ src/h06/PraktijkOpdracht.java
@@ -9,13 +9,21 @@ public class PraktijkOpdracht extends Applet {
     double cijfer2 = 6.3;
     double cijfer3 = 6.9;
 
+    double gemiddelde;
+    int intGemiddelde;
 
     public void init () {
-        setSize(400, 300);
+        setSize(350, 300);
+
+        gemiddelde = (cijfer1 + cijfer2 + cijfer3) / 3;
+        gemiddelde = gemiddelde * 10;
+        intGemiddelde = (int) gemiddelde;
+        gemiddelde = intGemiddelde;
+        gemiddelde = gemiddelde / 10;
     }
 
     public void paint (Graphics g) {
 
-
+        g.drawString("Het gemiddelde is: " + gemiddelde, 30, 50);
     }
 }
