a4f12ab9b5a445996c8ba0b16a140c06cb976075
oscar <oscarlemstra1@gmail.com>
2020-04-22 13:47:32 +0200

    opdr 8.1 af

--- src/h08/Opdr_8_1.java
+++ src/h08/Opdr_8_1.java
@@ -1,15 +1,54 @@
 package h08;
 
 import java.awt.*;
+import java.awt.event.*;
 import java.applet.*;
 
 public class Opdr_8_1 extends Applet {
 
+    Label label;
+    TextField tekstvak;
+    Button knopOk;
+    Button knopReset;
+
+    String input;
+    String output;
+
+
     public void init () {
         setSize(400, 300);
+
+        label = new Label("Type iets");
+        tekstvak = new TextField(input, 20);
+        knopOk = new Button ("Ok");
+        knopOk.addActionListener(new KnopListenerOk());
+        knopReset = new Button("Reset");
+        knopReset.addActionListener(new KnopListenerReset());
+
+        add(label);
+        add(tekstvak);
+        add(knopOk);
+        add(knopReset);
     }
 
     public void paint (Graphics g) {
+        g.drawString(output, 80, 70);
+    }
 
+    class KnopListenerOk implements ActionListener {
+
+        public void actionPerformed (ActionEvent e) {
+            input = tekstvak.getText();
+            output = input;
+            repaint();
+        }
+    }
+
+    class KnopListenerReset implements ActionListener {
+
+        public void actionPerformed (ActionEvent e) {
+            tekstvak.setText(input = "");
+            repaint();
+        }
     }
 }
