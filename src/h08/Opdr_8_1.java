d47a8334827b4f5a45c1c3ede23ef6652dea1b30
oscar <oscarlemstra1@gmail.com>
2020-04-22 11:42:19 +0200

    opdr 8.1 en test

--- src/h08/Opdr_8_1.java
+++ src/h08/Opdr_8_1.java
@@ -5,15 +5,8 @@ import java.applet.*;
 
 public class Opdr_8_1 extends Applet {
 
-    Button knop;
-
-
     public void init () {
         setSize(400, 300);
-
-        knop = new Button();
-        knop.setLabel( "Klik op mij" );
-        add(knop);
     }
 
     public void paint (Graphics g) {
