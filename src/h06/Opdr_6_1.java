8cabfc85770adfbfcd7cbb5a431f71d5d2963b0d
oscar <oscarlemstra1@gmail.com>
2020-04-23 18:13:21 +0200

    opdr 6.1 update

--- src/h06/Opdr_6_1.java
+++ src/h06/Opdr_6_1.java
@@ -17,9 +17,9 @@ public class Opdr_6_1 extends Applet {
 
     public void paint (Graphics g) {
 
-        g.drawString("Jan krijgt: € " + bedragPerPersoon + ",-", 30, 50);
-        g.drawString("Ali krijgt: € " + bedragPerPersoon + ",-", 30, 70);
-        g.drawString("Jeannette krijgt: € " + bedragPerPersoon + ",-", 30, 90);
-        g.drawString("Ik krijg: € " + bedragPerPersoon + ",-", 30, 110);
+        g.drawString("Jan krijgt: € " + bedragPerPersoon, 30, 50);
+        g.drawString("Ali krijgt: € " + bedragPerPersoon, 30, 70);
+        g.drawString("Jeannette krijgt: € " + bedragPerPersoon, 30, 90);
+        g.drawString("Ik krijg: € " + bedragPerPersoon, 30, 110);
     }
 }
