804abc93778245257be9fb500b952ef260715d43
oscar <oscarlemstra1@gmail.com>
2020-05-06 10:50:15 +0200

    opdr 10.1 af

--- src/h10/Opdr_10_1.java
+++ src/h10/Opdr_10_1.java
@@ -6,20 +6,39 @@ import java.applet.*;
 
 public class Opdr_10_1 extends Applet {
 
-    public TextField tekstvak;
+    Label label;
+    TextField tekstvak;
 
+    int getal, hoogsteGetal;
+    String input = "", output = "";
 
 
     public void init () {
         setSize(400, 300);
+        label = new Label("Type een getal in en druk op enter");
         tekstvak = new TextField("", 20);
+        tekstvak.addActionListener(new TekstvakLis());
 
+        add(label);
         add(tekstvak);
     }
 
     public void paint (Graphics g) {
 
+        output = "" + hoogsteGetal;
+        g.drawString("Hoogste getal is: " + output, 20, 100);
     }
 
+    class TekstvakLis implements ActionListener {
 
+        public void actionPerformed (ActionEvent e) {
+            input = tekstvak.getText();
+            getal = Integer.parseInt(input);
+
+            if (getal > hoogsteGetal) {
+                hoogsteGetal = getal;
+            }
+            repaint();
+        }
+    }
 }
